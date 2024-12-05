c73d1f9df5fbc133ff473ab699a4a9a7e58f5551
argonmining <ashton@onargon.com>
2024-12-04 19:07:03 -0800

    cleanup

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -13,24 +13,12 @@ export default function Footer() {
             <div className="mb-2">
               {/* Logo */}
               <Link className="inline-flex" href="/" aria-label="Cruip">
-                <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                  <defs>
-                    <linearGradient x1="0%" y1="32.443%" x2="104.18%" y2="50%" id="flogo-a">
-                      <stop stopColor="#FFF" stopOpacity=".299" offset="0%" />
-                      <stop stopColor="#7587E4" stopOpacity="0" offset="100%" />
-                    </linearGradient>
-                    <linearGradient x1="18.591%" y1="0%" x2="100%" y2="100%" id="flogo-b">
-                      <stop stopColor="#818CF8" offset="0%" />
-                      <stop stopColor="#C7D2FE" offset="100%" />
-                    </linearGradient>
-                  </defs>
-                  <g fill="none" fillRule="evenodd">
-                    <path fill="#3730A3" d="M16 18.5V32l15.999-9.25V9.25z" />
-                    <path fill="#4F46E5" d="m0 23 16 9V18.501L0 9.251z" />
-                    <path fillOpacity=".64" fill="url(#flogo-a)" d="M16 13 0 23l16 9 16-9z" />
-                    <path fill="url(#flogo-b)" d="M16 0 0 9.25l16 9.25 15.999-9.25z" />
-                  </g>
-                </svg>
+                <Image 
+                  src="/images/logo-image.png" 
+                  width={160} 
+                  height={160} 
+                  alt="Logo" 
+                />
               </Link>
             </div>
           </div>
@@ -121,9 +109,13 @@ export default function Footer() {
         </div>
         {/* Bottom area */}
         <div className="md:flex md:items-center md:justify-between pb-4 md:pb-8">
-          {/* Links */}
+          {/* Left-aligned text */}
           <div className="text-sm text-slate-600">
-            <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="https://x.com/argonmining">Built with  ❤️ by Ashton</a>
+            <span className="text-slate-500">© 2024 Kaspa DAO</span>
+          </div>
+          {/* Right-aligned text */}
+          <div className="text-sm text-slate-600 md:ml-auto">
+            <a className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out" href="https://x.com/argonmining">Built with ❤️ by Ashton</a>
           </div>
         </div>
       </div>
